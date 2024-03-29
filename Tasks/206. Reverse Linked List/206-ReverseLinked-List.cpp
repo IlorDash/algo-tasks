4e9a1977fc1e363865b98f50a5c7f0278aa8528e
Ilya Orazov <ilordash@mail.ru>
2024-03-30 00:53:53 +0300

    finish 206. Reverse Linked List - iteratively

--- Tasks/206. Reverse Linked List/206-ReverseLinked-List.cpp
+++ Tasks/206. Reverse Linked List/206-ReverseLinked-List.cpp
@@ -3,6 +3,8 @@
 
 #include "stdlib.h"
 
+#define RECURSIVE false
+
 struct ListNode* recursiveReverse(struct ListNode* prev) {
 	struct ListNode* node = prev->next;
 	struct ListNode* newHead;
@@ -18,11 +20,25 @@ struct ListNode* recursiveReverse(struct ListNode* prev) {
 	return newHead;
 }
 
+struct ListNode* iterativelyReverse(struct ListNode* prev) {
+	struct ListNode* node = prev->next;
+	struct ListNode* nextNode;
+	prev->next = NULL;
+	while (node->next != NULL) {
+		nextNode = node->next;
+		node->next = prev;
+		prev = node;
+		node = nextNode;
+	}
+	node->next = prev;
+	return node;
+}
+
 struct ListNode* reverseList(struct ListNode* head) {
 
 	if ((head == NULL) || (head->next == NULL)) {
 		return head;
 	}
 
-	return recursiveReverse(head);
+	return RECURSIVE ? (recursiveReverse(head)) : iterativelyReverse(head);
 }
